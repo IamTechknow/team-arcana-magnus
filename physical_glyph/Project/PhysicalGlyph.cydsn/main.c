0ca8b77da97a4e726d2ad0fe2a467e2eb19a3f7f
Edmond Lee <techhexium@gmail.com>
2018-04-30 11:32:12 -0700

    Start on PSOC project

---
+++ physical_glyph/Project/PhysicalGlyph.cydsn/main.c
@@ -0,0 +1,15 @@
+#include "project.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    for(;;)
+    {
+        /* Place your application code here. */
+    }
+}
+
+/* [] END OF FILE */
