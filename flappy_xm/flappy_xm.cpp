4d26a1ba60a4e89fe8f873671adb801ae46f9c23
Edmond Lee <techhexium@gmail.com>
2018-05-18 22:22:07 +0000

    Add makefile for tests, work on two player game

--- flappy_xm/flappy_xm.cpp
+++ flappy_xm/flappy_xm.cpp
@@ -1,6 +1,3 @@
-#include "led-matrix.h"
-#include "graphics.h"
-
 #include <string>
 #include <signal.h>
 #include <stdio.h>
@@ -13,17 +10,26 @@
 using namespace rgb_matrix;
 
 //Game states
-enum State {IDLE, GAME, WIN_ANIM};
+enum State {IDLE, GAME, WIN_ANIM, LOSE_ANIM};
+
+//Game colors
+Color cyan(0, 255, 255), purple(255, 0, 255);
 
 //Game settings
 State gameState = IDLE;
-float fps = 3.0;
-Color cyan(0, 255, 255);
+float fps = 3.0, text_fps = 7.0;
 float xmLeftY = 7.0, xmRightY = 7.0;
 float obstacleLeftX = 32.0F, obstacleRightX = 0;
 float gapLeftY = 8.0, gapRightY = 8.0;
-int delay_speed_usec = 1000000 / fps;
+int delay_usec = 1000000 / fps, text_delay = 1000000 / text_fps;
 int portalLevel = 0;
+int currFrames;
+Color color = cyan;
+
+//Text variables
+std::string idleLine = "Press buttons to play Flappy XM", gameOverStr = "Game over!", youWin = "You win!";
+Color bg_color(0, 0, 0);
+int x, x_orig;
 
 //Handle ctrl-C
 volatile bool interrupt_received = false;
@@ -31,25 +37,29 @@ static void InterruptHandler(int signo) {
   interrupt_received = true;
 }
 
-//Button interrupt
+//Button interrupt, triggers variables that get checked when FSM updates
+volatile bool buttonPressed = false, button2Pressed = false;
 void btnInt(int gpio, int level, uint32_t tick) {
     printf("Interrupt level %d at %u\n", level, tick);
 
-    if(gameState == IDLE && level == 1)
-		resetGame();
-    else if(level == 1) { //rising edge, did not timeout
+    if(level == 1) { //rising edge
         if(gpio == BTN1PIN)
-			jump1();
+			buttonPressed = true;
 		else
-			jump2();
+			button2Pressed = true;
 	}
 }
 
 //Callback for message arrival
 int msg_arrived(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
-	char *payload = (char*) message->payload;
-	char *end;
-	portalLevel = std::strtol(payload, &end, 10);
+	//Changes to portal level changes game settings. Only change during idle state for now
+	if(gameState == IDLE) {
+		char *payload = (char*) message->payload;
+		portalLevel = std::strtol(payload, NULL, 10); //Parse value, don't use end pointer
+
+		//Set color based on level
+		color = portalLevel == 8 ? purple : cyan;
+	}
 
 	MQTTClient_freeMessage(&message);
 	MQTTClient_free(topicName);
@@ -60,28 +70,41 @@ void conn_lost(void *context, char* cause) {
 	printf("Connection lost, cause: %s\n", cause);
 }
 
-void drawObstacles() {
+void drawObstacles(FrameCanvas* canvas) {
 	
+}
+
+//Set the pixels for the two XM particles
+void drawXM(FrameCanvas* canvas) {
+	canvas->SetPixel(XM1_X, (int) xmLeftY, color.r, color.g, color.b);
+	canvas->SetPixel(XM2_X, (int) xmRightY, color.r, color.g, color.b);
 }
 
 //Returns whether or not there is a collision and the game should end
 bool checkCollision() {
-	return xmLeftY < gapLeftY || xmLeftY > gapLeftY + 7 || xmRightY < gapRightY || xmRightY > gapRightY + 7;
+	return false; //TODO: Check whether an obstacle occupies the same spot as an XM particle
 }
 
+//Update game values that will be used to update the matrices
 void updateGame() {
-	xmLeftY += 1.0;
-	xmRightY += 1.0;
-	
+	xmLeftY += 0.5; //Move down players every 2/3 a second
+	xmRightY += 0.5;
+	//currFrames++;
+
 	if(xmLeftY >= ROWS || xmRightY >= ROWS)
 		gameOver();
 }
 
+//Start scrolling the Game over text
 void gameOver() {
-	gameState = IDLE;
+	printf("Game over\n");
+	gameState = LOSE_ANIM;
+	x = x_orig;
 }
 
+//Reset values and start a new game
 void resetGame() {
+	currFrames = 0;
 	xmLeftY = 7.0;
 	xmRightY = 7.0;
 	obstacleLeftX = 32.0;
@@ -89,8 +112,10 @@ void resetGame() {
 	gapLeftY = 8.0;
 	gapRightY = 8.0;
 	gameState = GAME;
+	printf("Starting new game...\n");
 }
 
+//Raise the position of the left and right players
 void jump1() {
 	if(xmLeftY > 0)
 		xmLeftY -= 0.5;
@@ -99,6 +124,47 @@ void jump1() {
 void jump2() {
 	if(xmRightY > 0)
 		xmRightY -= 0.5;
+}
+
+//Draw text on the screen and update the given x variable.
+int updateScrollText(FrameCanvas *canvas, Font font, int x, char *str) {
+	int length = DrawText(canvas, font, x, 0 + font.baseline(), color, &bg_color, str, 0);
+
+	if (--x + length < 0) 
+		x = x_orig;
+
+	return x;
+}
+
+void processState() {
+	State newState;
+	//Transition from the animation states to the idle state
+	if((gameState == LOSE_ANIM && x == -9) || (gameState == WIN_ANIM && x == -6)) //Text is going through left matrix
+		newState = IDLE;
+
+	//Manage button presses
+	if(buttonPressed) {
+		if(gameState == IDLE)
+			resetGame();
+		else if(gameState == GAME)
+			jump1();
+
+		buttonPressed = false;
+	}
+
+	if(button2Pressed) {
+		if(gameState == GAME)
+			jump2();
+
+		button2Pressed = false;
+	}
+
+	//Check win condition
+	if(gameState == GAME)
+		if((portalLevel < 8 && currFrames >= FRAMES_TO_WIN) || (portalLevel == 8 && currFrames >= FRAMES_TO_WIN_LVL8))
+			newState = WIN_ANIM;
+
+	gameState = newState;
 }
 
 int main(int argc, char **argv) {
@@ -110,9 +176,9 @@ int main(int argc, char **argv) {
 	gpioSetPullUpDown(BTN2PIN, PI_PUD_DOWN);
 	gpioSetISRFunc(BTN1PIN, RISING_EDGE, 0, btnInt);
 	gpioSetISRFunc(BTN2PIN, RISING_EDGE, 0, btnInt);
-	
+
 	//Init MQTT
-	MQTTClient client;
+/*	MQTTClient client;
 	MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
 
 	MQTTClient_create(&client, BROKER, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
@@ -126,47 +192,85 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 	MQTTClient_subscribe(client, TOPIC, QOS);
-
+*/
 	//Init Matrix
 	RGBMatrix::Options opts;
-	rgb_matrix::RuntimeOptions runtime_defaults;
-	
+
 	//Using 2 32x16 matrices, drop sudo
 	opts.hardware_mapping = "regular";
 	opts.chain_length = 2;
 	opts.rows = ROWS;
 	opts.cols = COLS;
 	opts.pwm_bits = 5; //less PWM bits = less CPU time
-	runtime_defaults.drop_privileges = 1;
-	
-	RGBMatrix *matrix = rgb_matrix::CreateMatrixFromOptions(opts, runtime_defaults);
-	if (matrix == NULL) {
+	x_orig = (opts.chain_length * opts.cols) + 5;
+	x = x_orig;
+
+	//Load Font
+	Font the_font;
+	if(!the_font.LoadFont("led_matrix/fonts/8x13.bdf")) {
+		printf("Couldn't load the specified font. Exiting...\n");
 		return 1;
 	}
-	
+
+	RuntimeOptions runtime_defaults;
+	runtime_defaults.drop_privileges = 1;
+
+	RGBMatrix *matrix = rgb_matrix::CreateMatrixFromOptions(opts, runtime_defaults);
+	if (matrix == NULL) {
+		printf("Matrix creation failed! Exiting...\n");
+		return 1;
+	}
+
 	signal(SIGTERM, InterruptHandler);
 	signal(SIGINT, InterruptHandler);
-	
+	printf("Program initialized. Running...\n");
+
 	// Create a new canvas to be used with led_matrix_swap_on_vsync
 	FrameCanvas *offscreen_canvas = matrix->CreateFrameCanvas();
-	
+
 	while (!interrupt_received) {
 		offscreen_canvas->Clear();
-		
-		//Draw current game data here!
-		//updateGame();
-		
-		usleep(delay_speed_usec);
-		
+
+		//Process the state machine
+		processState();
+		switch(gameState) {
+			case WIN_ANIM:
+				//x = updateScrollText(offscreen_canvas, the_font, x, youWin.c_str());
+				break;
+
+			case LOSE_ANIM:
+				//x = updateScrollText(offscreen_canvas, the_font, x, gameOverStr.c_str());
+				break;
+
+			case GAME:
+				updateGame();
+				drawXM(offscreen_canvas);
+				drawObstacles(offscreen_canvas);
+				break;
+			default:
+				//x = updateScrollText(offscreen_canvas, the_font, x, idleLine.c_str());
+				int length = rgb_matrix::DrawText(offscreen_canvas, the_font,
+                            x, 0 + the_font.baseline(),
+                            color, &bg_color,
+							idleLine.c_str(), 0);
+
+				if (--x + length < 0)
+      				x = x_orig;
+		}
+
 		// Swap the offscreen_canvas with canvas on vsync, avoids flickering
 		offscreen_canvas = matrix->SwapOnVSync(offscreen_canvas);
+
+		usleep(gameState == GAME ? delay_usec : text_delay / the_font.CharacterWidth('W'));
 	}
-	
-	MQTTClient_disconnect(client, TIMEOUT);
-	MQTTClient_destroy(&client);
+
+//	MQTTClient_disconnect(client, TIMEOUT);
+//	MQTTClient_destroy(&client);
+	printf("Exiting on Ctrl-C...\n");
 	matrix->Clear();
 	delete matrix;
 	gpioTerminate();
-	
+
 	return 0;
 }
+
--- flappy_xm/flappy_xm.h
+++ flappy_xm/flappy_xm.h
@@ -1,19 +1,33 @@
 #ifndef _FLAPPYXM_H_
 #define _FLAPPYXM_H_
 
+#include "led-matrix.h"
+#include "graphics.h"
+
 #define ROWS 16
 #define COLS 32
-#define BTN1PIN 28
+#define BTN1PIN 28 //BCM pins on Header P5
 #define BTN2PIN 29
+
 #define BROKER "192.168.1.124:1883"
 #define CLIENTID "FlappyClient"
 #define TOPIC "portal/levelChange"
 #define QOS 0
 #define TIMEOUT 10000L
 
-//Function prototypes
+#define XM1_X 8
+#define XM2_X 56
+#define FRAMES_TO_WIN 60 //20 seconds
+#define FRAMES_TO_WIN_LVL8 120
 
-void drawObstacle();
+using namespace rgb_matrix;
+
+//Function prototypes
+void processState();
+
+void drawObstacles(FrameCanvas *canvas);
+
+void drawXM(FrameCanvas *canvas);
 
 bool checkCollision();
 
@@ -27,4 +41,7 @@ void gameOver();
 
 void resetGame();
 
+int updateScrollText(FrameCanvas *canvas, Font font, int x, char *str);
+
 #endif
+
