5e558702f8a402140ed2312499df426669eae48a
Mathieu PLASSE <mathieu.plasse@gmail.com>
2018-05-20 15:46:37 +0000

    Isolates Button management code

--- passcode_decipher/ButtonEncoder.py
+++ passcode_decipher/ButtonEncoder.py
@@ -13,10 +13,18 @@ class ButtonWorker(object):
         self.gpio.setmode(GPIO.BCM)
         
         self.gpio.setup(self.pin, self.gpio.IN, pull_up_down=self.gpio.PUD_UP)
-        self.gpio.add_event_detect(self.pin, GPIO.RISING, callback=self.Call, bouncetime=50)
+        self.gpio.add_event_detect(self.pin, GPIO.RISING, callback = self.call, bouncetime = 50)
         
-    def Call(self, channel):
+    def call(self):
         state = self.gpio.input(self.pin)
         self.queue.put(EventClick)
         
-    
+    def process(self):
+        result = False
+        while not (self.queue.empty()):
+            m = self.queue.get_nowait()
+            if m == ButtonEncoder.EventClick:
+                print ("Clicked")
+                result =  True
+            self.queue.task_done()
+        return result
--- passcode_decipher/run.py
+++ passcode_decipher/run.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     pygame.display.set_caption(Consts.TITLE)
     screen = pygame.display.set_mode((Consts.screen_width, Consts.screen_height), 0, 32)
     #screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, 32)
-    background = pygame.transform.smoothscale(Consts.IMAGESDICT['board'], (Consts.screen_width, Consts.screen_height))
+
     fpsClock = pygame.time.Clock()
     GPIO.setmode(GPIO.BCM)
     
@@ -31,9 +31,12 @@ if __name__ == "__main__":
     #wheel.rect.x = 200
     #wheel.rect.y = 200
     #all_sprites_list.add(wheel)
-    background = pygame.transform.smoothscale(Consts.IMAGESDICT['board'], (Consts.screen_width, Consts.screen_height))
-    fpsClock = pygame.time.Clock()
     
+    background = pygame.transform.smoothscale(Consts.IMAGESDICT['board'], (Consts.screen_width, Consts.screen_height))
+    screen.blit(background, [0, 0])
+    
+    
+    fpsClock = pygame.time.Clock()
 
     cypher = Algorithm()
     algos = cypher.get_algo_list()
@@ -42,18 +45,7 @@ if __name__ == "__main__":
     chooser_2 = AlgorithmChooser(screen, Consts.R2_PIN, Consts.POS_LIST_2, algos)
     chooser_3 = AlgorithmChooser(screen, Consts.R3_PIN, Consts.POS_LIST_3, algos)
 
-    switch_1_queue = Queue()
-    switch_1 = ButtonEncoder.ButtonWorker(Consts.S1_PIN, switch_1_queue)
-    
-    def processBtnQueue(btn_queue):
-        result = False
-        while not (btn_queue.empty()):
-            m = btn_queue.get_nowait()
-            if m == ButtonEncoder.EventClick:
-                print ("Clicked")
-                result =  True
-            btn_queue.task_done()
-        return result
+    switch_1 = ButtonEncoder.ButtonWorker(Consts.S1_PIN, Queue())
    
     try:
         global cur_sel_algo
@@ -67,7 +59,7 @@ if __name__ == "__main__":
             chooser_1.update()
             chooser_2.update()
             chooser_3.update()
-            if (processBtnQueue(switch_1_queue)):
+            if (switch_1.process()):
                 print("Selected Algo1 : " + str(chooser_1.position) + " - " + str(algos[chooser_1.position]) \
                       + " against used Algo1 : " + str(used_algo_id) + " - " + str(algos[used_algo_id]))
                 print("Selected Algo2 : " + str(chooser_2.position) + " - " + str(algos[chooser_2.position]) \
