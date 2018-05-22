c302ddc020a43279faa536baee08e1a22a302fd9
Edmond Lee <techhexium@gmail.com>
2018-05-22 04:17:43 +0000

    Adjust the fading on the PAR lights

--- tecthultu/lighting_ctrl.py
+++ tecthultu/lighting_ctrl.py
@@ -17,14 +17,14 @@ def DmxHandler(status):
         print('Error: ' + status.message)
 
 #Send DMX data to the PAR lights based on the MQTT payload
-#Levels 0, 1, 4, and 8 are fade colors
+#Levels 0 and 8 are fade colors
 def ctrlLightsByLevel(level):
-    data = [[140, 100, 192] + [0] * 5, [140, 70, 192] + [0] * 5, 
-	[0, 110, 192, 255, 255, 0xA6, 0x30, 0], [0, 170, 192, 255, 255, 0x73, 0x15, 0], 
-	[140, 40, 192] + [0] * 5, [0, 120, 192, 255, 0xFD, 0x29, 0x92, 0], 
-	[0, 120, 192, 255, 0xEB, 0x26, 0xCD, 0], [0, 120, 192, 255, 0xC1, 0x24, 0xE0, 0], [140, 90, 192] + [0] * 5]
+    data = [[140, 100, 80] + [0] * 5, [11, 70, 192] + [0] * 5,
+	[0, 120, 192, 255, 255, 0xA6, 0x30, 0], [0, 120, 192, 255, 255, 0x73, 0x15, 0],
+	[11, 40, 64] + [0] * 5, [0, 120, 192, 255, 0xFD, 0x29, 0x92, 0],
+	[0, 120, 192, 255, 0xEB, 0x26, 0xCD, 0], [0, 120, 192, 255, 0xC1, 0x24, 0xE0, 0], [140, 90, 80] + [0] * 5]
     arr = array.array('B', data[level] + [0] * 504)
-    
+
     client.SendDmx(0, arr, DmxHandler)
 
 def turnOffLights():
@@ -32,7 +32,6 @@ def turnOffLights():
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    #In Python 2.7, self refers to the client object
     client.subscribe("portal/#")
 
 # The callback for when a PUBLISH message is received from the server.
@@ -49,6 +48,6 @@ if __name__ == '__main__':
     mqttClient.on_message = on_message
     mqttClient.connect(MQTT_BROKER, 1883, 60)
     try:
-    	mqttClient.loop_forever()
+        mqttClient.loop_forever()
     finally:
-	turnOffLights()
+        turnOffLights()
